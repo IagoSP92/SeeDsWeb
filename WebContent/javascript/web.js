65f58c282d1111c2c06b66d3ead69b664a5ab9f8
usuario <usuario@usuario-PC>
2019-05-01 12:21:03 +0200

    limpieza js

--- WebContent/javascript/web.js
+++ WebContent/javascript/web.js
@@ -199,7 +199,6 @@ $(document).ready(function () {
 	});
 	
 	checkCompatible();
-	divCheck();
 
 	$("#checkTodos").change(function(){
 
@@ -213,11 +212,7 @@ function checkCompatible (){
 		$("#checkUsuarios").prop('checked', true);
 	}
 }
-/*
-function divCheck (){
-	$("#checkTodosDiv").click(toggleCheck("#checkTodos"));
 
-}*/
 
 function toggleCheck(check){
 	if( $(check).prop('checked')==true ){
@@ -229,63 +224,3 @@ function toggleCheck(check){
 
 
 
-
-
-
-// PABLO  -   PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  - 
-//	 	PABLO  -   PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  - 
-// PABLO  -   PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  - 
-// 		PABLO  -   PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  -  PABLO  - 
-
-/*
-//Menu desplegable
-$(document).ready(function () { 
-
-	$("#paises").change(function(){
-		var selectedCountry = $(this).children("option:selected").val();
-		$.ajax({
-			type: "GET",
-			url: "/PulBetWeb/usuario",
-			data: { 'id':selectedCountry,
-				'action':"preRegistro"},
-				contentType:"application/x-www-form-urlencoded; charset=ISO-8859-1",
-				dataType:"json",
-				success: function (provinciasArray) {
-					
-					if(!$("#provincias").html().isEmpty){
-						$("#provincias").html("");
-					}
-					
-					for(var i = 0; i < provinciasArray.length; i++){
-						$("#provincias").html($("#provincias").html()+"<option value="+provinciasArray[i].id+">"+provinciasArray[i].nome+"</option>")
-
-					}
-
-				}
-		});
-
-	})
-	var x = ($("#acumulada").val())*($("#importe").val());
-	$("#ganancias").val(x);
-
-});
-
-/*
-function desplegarMenu() {
-	document.getElementById("meumenudes").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-	if (!event.target.matches('.dropbtn')) {
-
-		var menudess = document.getElementsByClassName("menudes-contido");
-		var i;
-		for (i = 0; i < menudess.length; i++) {
-			var openmenudes = menudess[i];
-			if (openmenudes.classList.contains('show')) {
-				openmenudes.classList.remove('show');
-			}
-		}
-	}
-}
-*/
